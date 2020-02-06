e24a36109d6cc2f5a1498cb71fd73c8ab2c35247
megavolt2000 <megavolt2000@gmail.com>
2020-02-06 21:22:46 +0000

    Comment

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
-alert('Hello JavaScript');
+// alert('Hello JavaScript');
 
-const greetings = 'Nice to see you!';
-alert(greetings);
+// const greetings = 'Nice to see you!';
+// alert(greetings);
 
-let name = 'John';
-alert(name);
+// let name = 'John';
+// alert(name);
 
-name = 'Isabel';
-alert(name);
+// name = 'Isabel';
+// alert(name);
 
-const userName = prompt('Enter your name');
-const userGreetings = 'Nice to see you, ' + userName;
-alert(userGreetings);
+// const userName = prompt('Enter your name');
+// const userGreetings = 'Nice to see you, ' + userName;
+// alert(userGreetings);
 
 const userAge = parseFloat(prompt('Enter your age'));
-const nextAge = add(userAge + 1);
+const nextAge = add(userAge, 1);
 const ageMessage = 'Next year you will be ' + nextAge;
 alert(ageMessage);
 
